841f35917812ac1ce893dd72d8093d58cc545b2a
Janhavi Sawant <janhavisawant822008@gmail.com>
2026-07-01 21:08:51 +0530

    Added arithmetic program

---
+++ arithmetic.py
@@ -0,0 +1,9 @@
+no1=int(input("Enter first number"))#4
+no2=int(input("Enter second number"))#2
+print("Addition of two number=",no1+no2)#4+2=6
+print("substraction of two number=",no1-no2)#4-2=2
+print("Multiplication of two number=",no1*no2)#4*2=8
+print("Division of two number=",no1/no2)#4/2=2
+print("Modulus of two number=",no1%no2)#4%2=0
+print("Floor division of two number=",no1//no2)#4//2=2
+print("Exponent of two number=",no1**no2)#4**2=16
