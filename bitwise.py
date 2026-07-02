4bb4e350bb5fc793115dce8ed02874fe07caeee9
Janhavi Sawant <janhavisawant822008@gmail.com>
2026-07-02 22:04:42 +0530

    Added bitwise program

---
+++ bitwise.py
@@ -0,0 +1,20 @@
+a = 10   # Binary: 1010
+b = 4    # Binary: 0100
+
+# Bitwise AND (&)
+print("a & b =", a & b)
+
+# Bitwise OR (|)
+print("a | b =", a | b)
+
+# Bitwise XOR (^)
+print("a ^ b =", a ^ b)
+
+# Bitwise NOT (~)
+print("~a =", ~a)
+
+# Left Shift (<<)
+print("a << 1 =", a << 1)
+
+# Right Shift (>>)
+print("a >> 1 =", a >> 1)
