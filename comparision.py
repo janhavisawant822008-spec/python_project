bd581a44156296609a923befae7b6a898789c8a5
Janhavi Sawant <janhavisawant822008@gmail.com>
2026-07-02 22:33:19 +0530

    Added comparision program

---
+++ comparision.py
@@ -0,0 +1,8 @@
+no1=int(input("Enter first number"))#20
+no2=int(input("Enter second number"))#10
+print("no1 is greater no2=",no1>no2)#20>10 true
+print("no1 is less no2=",no1<no2)#20<10 #false
+print("no1 is greater or equal no2=",no1>=no2)#20>=10  truue
+print("no1 is less or equal to  no2=",no1<=no2)#20<=10 false
+print("no1 is equal no2=",no1==no2)#20==10 false
+print("no1 is not equal no2=",no1!=no2)#20!=10 true
