457d80323d5649291d5ddc72d37d094cd84b161c
Janhavi Sawant <janhavisawant822008@gmail.com>
2026-07-02 21:39:38 +0530

    Added list_2 program

---
+++ list_2.py
@@ -0,0 +1,35 @@
+'''#append banana apple
+fruit=["apple,banana"]
+fruit.append("banana")
+print(fruit) '''
+
+'''#insert
+fruit=["apple","orange"]
+fruit.insert(0,"banana")
+print(fruit)'''
+
+'''#remove 
+fruit=["apple","orange","banana"]
+fruit.remove("apple")
+print(fruit)'''
+
+'''#extend
+fruit=["apple","orange","banana"]
+no=[1,2,3]
+fruit.extend(no)
+print(fruit)'''
+
+
+'''fruit=["apple","orange","banana"]
+fruit.pop()
+print(fruit)'''
+
+'''fruit=["apple","orange","banana"]
+fruit.clear()
+print(fruit)'''
+
+number=[20,40,90,10] #10 20 40 90  reverse
+number.sort() #10 20 40 90
+print(number)
+number.reverse()#90 40 20 10
+print(number)
