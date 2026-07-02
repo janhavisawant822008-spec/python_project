387a921ed9332c7e867108471ac4adbca179d7d8
Janhavi Sawant <janhavisawant822008@gmail.com>
2026-07-02 15:01:32 +0530

    Added list_1 program

---
+++ list_1.py
@@ -0,0 +1,29 @@
+#1st list program
+'''fruit=["banana",2,"apple"]
+print(fruit)
+
+fruit=["apple","banana","orange"]
+print(fruit[2])#apple
+
+#negative index
+fruit=["apple","banana","orange"]
+print(fruit[-3])#apple
+
+
+#slicing
+fruit=["apple","banana","orange","rose"]
+print(fruit[0:3])'''
+
+'''#mutable-change value list
+#immutable-you cant change value 
+fruit=["apple","banana","orange","rose"]
+fruit[0]="Lily"
+print(fruit)#Lily banana'''
+
+
+'''fruit="janhavi sawant"
+fruit[0]="c"
+print(fruit)'''
+
+list=[35,42,23]
+print(len(list))
