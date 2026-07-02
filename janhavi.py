fb69d75a5b3a3f5682c2fcc6be39443c2af91d11
Janhavi Sawant <janhavisawant822008@gmail.com>
2026-07-02 14:28:22 +0530

    Added janhavi program

---
+++ janhavi.py
@@ -0,0 +1,10 @@
+age=18
+marks=90.67
+name="Janhavi"
+vote=True
+
+
+print("age value=",type(age))
+print("marks value=",type(marks))
+print("name value=",type(name))
+print("vote value=",type(vote))
